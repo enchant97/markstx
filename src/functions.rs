43a135e2371f548445b71a6bed9b1593fffabc78
Leo Spratt <enchant97@users.noreply.github.com>
2024-01-07 18:02:56 +0000

    better templating & first macro

--- src/functions.rs
+++ src/functions.rs
@@ -1,50 +1,59 @@
-use std::{collections::HashMap, fs};
-use tera::{from_value, Function, Result, Value};
+use minijinja::{value::Kwargs, Error};
+use std::fs;
 
-pub fn make_csv_to_table() -> impl Function {
-    Box::new(move |args: &HashMap<String, Value>| -> Result<Value> {
-        match args.get("path") {
-            Some(val) => match from_value::<String>(val.clone()) {
-                Ok(path) => {
-                    let content = fs::read_to_string(path)?;
-                    let mut lines = content.lines();
-                    let mut output = String::new();
+pub fn lorem_ipsum(options: Kwargs) -> Result<String, Error> {
+    match options.get("words")? {
+        Some(words) => Ok(lipsum::lipsum(words)),
+        None => Err(Error::new(
+            minijinja::ErrorKind::MissingArgument,
+            "missing 'words'",
+        )),
+    }
+}
 
-                    output.push_str("\n<table>");
+pub fn csv_to_table(options: Kwargs) -> Result<String, Error> {
+    match options.get("path")? {
+        Some(path) => {
+            let content = fs::read_to_string::<&str>(path)
+                .map_err(|e| Error::new(minijinja::ErrorKind::InvalidOperation, e.to_string()))?;
+            let mut lines = content.lines();
+            let mut output = String::new();
 
-                    if let Some(header) = &lines.next() {
-                        let columns: Vec<&str> = header.split(',').collect();
-                        output.push_str("<thead>");
-                        output.push_str(
-                            &columns
-                                .into_iter()
-                                .map(|v| format!("<th>{}</th>", v.trim()))
-                                .collect::<Vec<_>>()
-                                .join(""),
-                        );
-                        output.push_str("</thead>");
-                    }
+            output.push_str("\n<table>");
 
-                    for row in lines.into_iter() {
-                        let columns: Vec<&str> = row.split(',').collect();
-                        output.push_str("<tr>");
-                        output.push_str(
-                            &columns
-                                .into_iter()
-                                .map(|v| format!("<td>{}</td>", v.trim()))
-                                .collect::<Vec<_>>()
-                                .join(""),
-                        );
-                        output.push_str("</tr>");
-                    }
+            if let Some(header) = &lines.next() {
+                let columns: Vec<&str> = header.split(',').collect();
+                output.push_str("<thead>");
+                output.push_str(
+                    &columns
+                        .into_iter()
+                        .map(|v| format!("<th>{}</th>", v.trim()))
+                        .collect::<Vec<_>>()
+                        .join(""),
+                );
+                output.push_str("</thead>");
+            }
 
-                    output.push_str("</table>\n");
+            for row in lines.into_iter() {
+                let columns: Vec<&str> = row.split(',').collect();
+                output.push_str("<tr>");
+                output.push_str(
+                    &columns
+                        .into_iter()
+                        .map(|v| format!("<td>{}</td>", v.trim()))
+                        .collect::<Vec<_>>()
+                        .join(""),
+                );
+                output.push_str("</tr>");
+            }
 
-                    Ok(output.into())
-                }
-                Err(_) => Err("oops".into()),
-            },
-            None => Err("missing 'path'".into()),
+            output.push_str("</table>\n");
+
+            Ok(output.into())
         }
-    })
+        None => Err(Error::new(
+            minijinja::ErrorKind::MissingArgument,
+            "missing 'path'",
+        )),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,35 @@ mod functions;
 
 use args::Args;
 use clap::Parser;
+use minijinja::Environment;
 use pulldown_cmark::Options;
 use rust_embed::RustEmbed;
-use tera::Tera;
 
 #[derive(RustEmbed)]
 #[folder = "templates/"]
-struct TemplateAsset;
+struct TemplateAssets;
+
+fn create_template_env() -> Environment<'static> {
+    let mut env = Environment::new();
+    env.set_loader(|name| {
+        if let Some(asset) = TemplateAssets::get(&format!("{name}.html.tmpl")) {
+            return Ok(std::str::from_utf8(&asset.data).ok().map(|v| v.to_owned()));
+        }
+        Ok(None)
+    });
+    env.add_function("csv_to_table", functions::csv_to_table);
+    env.add_function("lorem_ipsum", functions::lorem_ipsum);
+    env
+}
 
 fn main() {
     let args = Args::parse();
 
-    let templates = TemplateAsset::iter().map(|filename| {
-        (
-            filename.to_string(),
-            TemplateAsset::get(&filename).expect(&format!("failed to load asset '{}'", filename)),
-        )
-    });
-    let mut tera = Tera::default();
-    for template in templates {
-        tera.add_raw_template(
-            &template.0,
-            std::str::from_utf8(&template.1.data).expect("failed to convert file into utf8"),
-        )
-        .unwrap();
-    }
-    tera.register_function("csv_to_table", functions::make_csv_to_table());
-    tera.add_template_file(&args.file, None).unwrap();
+    let template_env = create_template_env();
 
-    let enriched_md = tera
-        .render(args.file.to_str().unwrap(), &tera::Context::new())
-        .unwrap();
+    let source_content = std::fs::read_to_string(args.file.to_str().unwrap()).unwrap();
+
+    let enriched_md = template_env.render_str(&source_content, {}).unwrap();
 
     let md_parser = pulldown_cmark::Parser::new_ext(&enriched_md, Options::all());
     let mut output = String::new();
